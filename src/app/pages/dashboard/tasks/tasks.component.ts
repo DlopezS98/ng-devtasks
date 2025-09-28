c4e29841174a28c61772dad432a835fe94d71fb6
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 11:14:22 -0600

    initial kanban setup

--- src/app/pages/dashboard/tasks/tasks.component.ts
+++ src/app/pages/dashboard/tasks/tasks.component.ts
@@ -1,12 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TaskResponseDto, TaskStatuses } from '../../../shared/models/task.model';
+import { KanbanService } from '../../../shared/services/kanban.service';
 
 @Component({
   selector: 'app-tasks.component',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatListModule, MatToolbarModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TasksComponent {
+  statuses = Object.values(TaskStatuses);
+  tasksByStatus: { [key: string]: TaskResponseDto[] } = {};
 
+  constructor(private kanbanService: KanbanService) {
+    this.statuses.forEach(status => {
+      this.tasksByStatus[status] = this.kanbanService.getTasksByStatus(status as TaskStatuses);
+    });
+  }
 }
