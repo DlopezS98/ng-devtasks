393383e276657c3c83c4041b60061a37b7c49843
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 11:47:29 -0600

    fix: update TaskStatuses to TaskStatus in TasksComponent for consistency and import KanbanColumnComponent

--- src/app/pages/dashboard/tasks/tasks.component.ts
+++ src/app/pages/dashboard/tasks/tasks.component.ts
@@ -4,24 +4,26 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { TaskResponseDto, TaskStatuses } from '../../../shared/models/task.model';
+import { TaskResponseDto, TaskStatus } from '../../../shared/models/task.model';
 import { KanbanService } from '../../../shared/services/kanban.service';
+import { KanbanColumnComponent } from './components/kanban-column/kanban-column.component';
 
 @Component({
   selector: 'app-tasks.component',
-  imports: [MatCardModule, MatChipsModule, MatListModule, MatToolbarModule, CommonModule],
+  imports: [MatCardModule, MatChipsModule, MatListModule, MatToolbarModule, CommonModule, KanbanColumnComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss',
+  styleUrls: ['./tasks.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
 })
 export class TasksComponent {
-  statuses = Object.values(TaskStatuses);
+  statuses = Object.values(TaskStatus);
   tasksByStatus: { [key: string]: TaskResponseDto[] } = {};
 
   constructor(private kanbanService: KanbanService) {
     this.statuses.forEach(status => {
-      this.tasksByStatus[status] = this.kanbanService.getTasksByStatus(status as TaskStatuses);
+      this.tasksByStatus[status] = this.kanbanService.getTasksByStatus(status as TaskStatus);
     });
   }
 }
