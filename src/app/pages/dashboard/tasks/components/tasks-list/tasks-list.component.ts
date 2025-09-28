3b91946969042a3eb57d8a32236cb28551676ae0
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 16:53:39 -0600

    feat: implement tasks list component with loading state and pagination

--- src/app/pages/dashboard/tasks/components/tasks-list/tasks-list.component.ts
+++ src/app/pages/dashboard/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,11 +1,40 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { CommonModule, DatePipe } from "@angular/common";
+import { AfterViewInit, Component, inject, signal, ViewChild } from "@angular/core";
+import { MatTableModule } from "@angular/material/table";
+import { Task } from "../../../../../shared/models/task.model";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatSort, MatSortModule } from "@angular/material/sort";
+import { merge, startWith, switchMap } from "rxjs";
+import { KanbanService } from "../../../../../shared/services/kanban.service";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.scss'],
-  standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule, MatSortModule, MatPaginatorModule, DatePipe],
 })
-export class TasksListComponent {}
+export class TasksListComponent implements AfterViewInit{
+  displayedColumns: string[] = ['title', 'status', 'createdAt', 'completedAt'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource = signal<Task[]>([]);
+  totalCount = signal(0);
+  isLoadingResults = signal(true);
+  tasksService = inject(KanbanService);
+
+  ngAfterViewInit(): void {
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    merge(this.sort.sortChange, this.paginator.page).pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults.set(true);
+        return this.tasksService.searchTasks$();
+      })
+    ).subscribe((result) => {
+      this.dataSource.set(result.items);
+      this.totalCount.set(result.totalCount);
+      this.isLoadingResults.set(false);
+    });
+  }
+}
