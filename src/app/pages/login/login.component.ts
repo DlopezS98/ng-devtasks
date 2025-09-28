991e9c228393367aa33f8cfc5940b1007717d935
DlopezS98 <01dlopezs98@gmail.com>
2025-09-27 22:02:58 -0600

    feat: implement login form with validation and authentication logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthStore } from '../../shared/state/auth.state';
 
 @Component({
-  selector: 'app-login.component',
-  imports: [],
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public readonly form: FormGroup<{
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
 
+  constructor(
+    // private readonly authService: AuthenticationService,
+    private readonly authStore: AuthStore,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  public login() {
+    console.log(this.form);
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.getRawValue();
+    this.authStore
+      .login(email!, password!)
+      .then(() => {
+        this.router.navigateByUrl('/dashboard');
+      })
+      .catch((error) => {
+        console.error('Login failed', error);
+      });
+  }
 }
