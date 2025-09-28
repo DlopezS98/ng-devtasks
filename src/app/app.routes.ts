25aac51c34aa602279ad9473ec0ab15953403a20
DlopezS98 <01dlopezs98@gmail.com>
2025-09-27 20:27:31 -0600

    feat: update routing to lazy load dashboard and redirect to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'login', component: LoginComponent }
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES) }
 ];
