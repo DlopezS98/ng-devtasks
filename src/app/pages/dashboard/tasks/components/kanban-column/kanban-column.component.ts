61b95c198d4a6f97728a7f7aa6bfc42704f7d2db
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 12:18:05 -0600

    feat: enhance KanbanColumn component with task display and styling improvements

--- src/app/pages/dashboard/tasks/components/kanban-column/kanban-column.component.ts
+++ src/app/pages/dashboard/tasks/components/kanban-column/kanban-column.component.ts
@@ -1,21 +1,39 @@
-import { Component, input, InputSignal, computed } from '@angular/core';
+import { Component, input, InputSignal, computed, effect, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { TaskStatus } from '../../../../../shared/models/task.model';
+import { TaskResponseDto, TaskStatus } from '../../../../../shared/models/task.model';
 import { MatIconModule } from '@angular/material/icon';
+import { KanbanService } from '../../../../../shared/services/kanban.service';
+import { MatChipsModule } from '@angular/material/chips';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-kanban-column',
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatChipsModule, CommonModule, MatButtonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './kanban-column.component.html',
-  styleUrls: ['./kanban-column.component.scss']
+  styleUrls: ['./kanban-column.component.scss'],
 })
 export class KanbanColumnComponent {
   status: InputSignal<TaskStatus> = input.required();
-  private iconsMap = {
+  private readonly taskTitle = {
+    [TaskStatus.Draft]: 'Drafts',
+    [TaskStatus.ToDo]: 'To Do',
+    [TaskStatus.InProgress]: 'In Progress',
+    [TaskStatus.Done]: 'Completed',
+  };
+  private readonly iconsMap = {
     [TaskStatus.Draft]: 'drafts',
     [TaskStatus.ToDo]: 'list_alt',
     [TaskStatus.InProgress]: 'autorenew',
-    [TaskStatus.Done]: 'check_circle'
-  }
+    [TaskStatus.Done]: 'check_circle',
+  };
   icon = computed(() => this.iconsMap[this.status()]);
+  title = computed(() => this.taskTitle[this.status()]);
+  tasks: TaskResponseDto[] = [];
+  constructor(private kanbanService: KanbanService) {
+    effect(() => {
+      this.tasks = this.kanbanService.getTasksByStatus(this.status());
+    });
+  }
 }
