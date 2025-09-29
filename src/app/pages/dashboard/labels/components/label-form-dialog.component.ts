350f660fe6c88c878bc5dca6ae488f805d87fab9
DlopezS98 <01dlopezs98@gmail.com>
2025-09-29 01:02:49 -0600

    feat: add random color option to label creation form and update color handling

--- src/app/pages/dashboard/labels/components/label-form-dialog.component.ts
+++ src/app/pages/dashboard/labels/components/label-form-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,7 +17,8 @@ import { CreateLabel, Label } from '../../../../shared/models/task.model';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogModule,
+  MatDialogModule,
+  MatCheckboxModule,
   ],
   templateUrl: './label-form-dialog.component.html',
   styleUrls: ['./label-form-dialog.component.scss'],
@@ -24,6 +26,7 @@ import { CreateLabel, Label } from '../../../../shared/models/task.model';
 export class LabelFormDialogComponent {
   form: FormGroup;
   isEdit: boolean;
+  randomColor = false;
 
   constructor(
     private fb: FormBuilder,
@@ -33,13 +36,18 @@ export class LabelFormDialogComponent {
     this.isEdit = !!data.label;
     this.form = this.fb.group({
       name: [data.label?.name || ''],
-      color: [data.label?.color],
+      color: [data.label?.color || '#000000'],
     });
   }
 
   submit() {
     if (this.form.valid) {
-      this.dialogRef.close(this.form.value as CreateLabel);
+      let value = this.form.value;
+      if (!this.isEdit && this.randomColor) {
+        // Generate random color and do not send color to API
+        value = { name: value.name };
+      }
+      this.dialogRef.close(value as CreateLabel);
     }
   }
 
