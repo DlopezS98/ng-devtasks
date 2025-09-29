b9d2812da4701e670c867cb2eec593c8323d0fd8
DlopezS98 <01dlopezs98@gmail.com>
2025-09-29 00:36:15 -0600

    intial setup for labels

--- src/app/pages/dashboard/labels/labels.component.ts
+++ src/app/pages/dashboard/labels/labels.component.ts
@@ -1,11 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { LabelsService } from '../../../shared/services/labels.service';
+import { Label, CreateLabel, UpdateLabel } from '../../../shared/models/task.model';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-labels.component',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './labels.component.html',
   styleUrl: './labels.component.scss'
 })
 export class LabelsComponent {
+  private labelsService = inject(LabelsService);
+  private fb = inject(FormBuilder);
 
+  labels = signal<Label[]>([]);
+  form: FormGroup;
+  editingLabel: Label | null = null;
+
+  constructor() {
+    this.form = this.fb.group({
+      name: [''],
+      color: [''],
+    });
+    this.loadLabels();
+  }
+
+  loadLabels() {
+    this.labelsService.getLabels$().subscribe(labels => {
+      this.labels.set(labels);
+    });
+  }
+
+  submit() {
+    if (this.editingLabel) {
+      const update: UpdateLabel = {
+        id: this.editingLabel.id,
+        ...this.form.value
+      };
+      this.labelsService.updateLabel$(update).pipe(
+        tap(() => {
+          this.editingLabel = null;
+          this.form.reset();
+          this.loadLabels();
+        })
+      ).subscribe();
+    } else {
+      const create: CreateLabel = this.form.value;
+      this.labelsService.createLabel$(create).pipe(
+        tap(() => {
+          this.form.reset();
+          this.loadLabels();
+        })
+      ).subscribe();
+    }
+  }
+
+  edit(label: Label) {
+    this.editingLabel = label;
+    this.form.patchValue({
+      name: label.name,
+      color: label.color || ''
+    });
+  }
+
+  delete(label: Label) {
+    this.labelsService.deleteLabel$(label.id).pipe(
+      tap(() => this.loadLabels())
+    ).subscribe();
+  }
+
+  cancelEdit() {
+    this.editingLabel = null;
+    this.form.reset();
+  }
 }
