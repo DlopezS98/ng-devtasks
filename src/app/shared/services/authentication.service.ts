2d76fa67b50887e91013a9d89fd9cfe600dc23c7
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 21:43:20 -0600

    refactor: remove fake user implementation and integrate real API calls for login and token validation

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -1,33 +1,31 @@
-import { Injectable } from "@angular/core";
-import { User } from "../models/user.model";
-import { delay, Observable, of } from "rxjs";
-import { TokenResponse } from "../models/token.model";
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { map, Observable, switchMap } from 'rxjs';
+import { TokenResponse } from '../models/token.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@env/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
-  private fakeUser : User = {
-    id: '1',
-    email: 'user@example.com',
-    displayName: 'John Doe',
-    createdAt: new Date(),
-  }
+  private readonly apiUrl = environment.apiUrl;
 
-  validateToken(token: string): Observable<User | null> {
-    return of(this.fakeUser).pipe(delay(500));
-  }
+  constructor(private readonly http: HttpClient) {}
 
-  login(email: string, password: string): Observable<{ token: TokenResponse, user: User } | null> {
-    const token: TokenResponse = {
-      accessToken: 'fake-jwt-token',
-      refreshToken: 'fake-refresh-token',
-      tokenExpiresAt: new Date(Date.now() + 3600 * 1000), // 1 hour
-      refreshTokenExpiresAt: new Date(Date.now() + 7 * 24 * 3600 * 1000), // 7 days
-    };
-    // retrieve the token response and then the user info (two different endpoints in a real app)
-    return of({ token, user: this.fakeUser }).pipe(delay(500));
+  login(email: string, password: string): Observable<{ token: TokenResponse; user: User } | null> {
+    const loginObs = this.http.post<TokenResponse>(`${this.apiUrl}/authentication/login`, {
+      email,
+      password,
+    });
+    return loginObs.pipe(
+      switchMap((token) =>
+        this.getUserFromToken(token.accessToken).pipe(
+          map((user) => (user ? { token, user } : null))
+        )
+      )
+    );
   }
 
   getUserFromToken(token: string): Observable<User | null> {
-    return of(this.fakeUser).pipe(delay(500));
+    return this.http.post<User>(`${this.apiUrl}/authentication/validate-token`, { token });
   }
 }
