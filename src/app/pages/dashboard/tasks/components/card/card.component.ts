3a087782b45d93f54a1650d67cfc5aa787b74f02
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 18:07:14 -0600

    card diaglog placeholder

--- src/app/pages/dashboard/tasks/components/card/card.component.ts
+++ src/app/pages/dashboard/tasks/components/card/card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { Task } from '../../../../../shared/models/task.model';
 
 @Component({
   selector: 'app-card.component',
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule, CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent {
-
+  data = inject(MAT_DIALOG_DATA) as Task | null;
+  isEditing = signal(!!this.data?.id);
+  title = computed(() => this.isEditing() ? 'Edit Task' : 'Create New Task');
+  constructor() {
+    console.log('CardComponent data:', this.data);
+  }
 }
