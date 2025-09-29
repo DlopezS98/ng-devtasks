8257f81d4842d5d8ce08e9be22d7c06511d0b95a
DlopezS98 <01dlopezs98@gmail.com>
2025-09-28 18:53:55 -0600

    feat: add register route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './shared/guards/auth.guard';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'dashboard',
     loadChildren: () =>
